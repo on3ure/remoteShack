4deffd5a2ef0aa97459646873eaa278b5d8c46fb
Joeri Van Dooren <ure@on3ure.be>
2020-04-08 17:38:01 +0200

    base input test

--- input/input.pl
+++ input/input.pl
@@ -4,129 +4,24 @@
 use v5.10;
 use strict;
 use utf8;
-use Mojolicious::Lite;
 use RPi::Pin;
 use RPi::Const qw(:all);
 use Config::YAML;
 
-my $config = Config::YAML->new( config => "relay.yaml" );
+my $config = Config::YAML->new( config => "input.yaml" );
 
-# Mojo config
-my $mojoconfig = plugin Config => { file => 'mojo.conf' };
+my $input;
+    foreach my $input_key ( keys %{ $config->{input} } ) {
+        $input->{$input_key}
+            = RPi::Pin->new( $config->{input}->{$input_key}->{gpio} );
+        $input->{$input_key}->mode(INPUT);
+    }
 
-my $relay;
+while (1) {
 
-foreach my $relay_key ( keys %{ $config->{relay} } ) {
-    $relay->{$relay_key}
-        = RPi::Pin->new( $config->{relay}->{$relay_key}->{gpio} );
-    $relay->{$relay_key}->mode(OUTPUT);
-    if ( $config->{relay}->{$relay_key}->{state} eq 'low' ) {
-        $relay->{$relay_key}->write(LOW);
-    }
-    elsif ( $config->{relay}->{$relay_key}->{state} eq 'high' ) {
-        $relay->{$relay_key}->write(HIGH);
-    }
-    else {
-        print "config error\n";
-    }
-    print "Init relay ["
-        . $relay_key
-        . "] -> gpio -> ["
-        . $config->{relay}->{$relay_key}->{gpio}
-        . "] state ["
-        . $config->{relay}->{$relay_key}->{state}
-        . "] name ["
-        . $config->{relay}->{$relay_key}->{name} . "]\n";
+    foreach my $input_key ( keys %{ $config->{input} } ) {
+      my $state = $input->{$input_key}->read;
+      print $config->{input}->{$input_key}->{name} . " -> " . $state . "\n";
+
+    sleep(1);
 }
-
-get '/:nr/:state' => sub {
-    my $self = shift;
-
-    # reder when we are done
-    $self->render_later;
-
-    my $nr    = $self->stash('nr');
-    my $state = $self->stash('state');
-
-    if ( $config->{relay}->{$nr}->{gpio} ) {
-        if ( $state eq 'high' ) {
-            $relay->{$nr}->write(HIGH);
-            my $data = {
-                'error' => 0,
-                'state' => 'high',
-                'name'  => $config->{relay}->{$nr}->{name}
-            };
-            $self->render( json => $data );
-        }
-        elsif ( $state eq 'low' ) {
-            $relay->{$nr}->write(LOW);
-            my $data = {
-                'error' => 0,
-                'state' => 'low',
-                'name'  => $config->{relay}->{$nr}->{name}
-            };
-            $self->render( json => $data );
-        }
-        else {
-            my $data = {
-                'error' => 1,
-                'state' => 'wrong state',
-                'name'  => $config->{relay}->{$nr}->{name}
-            };
-            $self->render( json => $data );
-        }
-    }
-    else {
-        my $data = {
-            'error' => 1,
-            'state' => 'not found'
-        };
-        $self->render( json => $data );
-    }
-};
-
-get '/:nr' => sub {
-    my $self = shift;
-
-    # reder when we are done
-    $self->render_later;
-
-    my $nr = $self->stash('nr');
-
-    if ( $config->{relay}->{$nr}->{gpio} ) {
-        my $read_state = $relay->{$nr}->read();
-        if ( $read_state eq '0' ) {
-            my $data = {
-                'error' => 0,
-                'state' => 'low',
-                'name'  => $config->{relay}->{$nr}->{name}
-            };
-            $self->render( json => $data );
-        }
-        elsif ( $read_state eq '1' ) {
-            my $data = {
-                'error' => 0,
-                'state' => 'high',
-                'name'  => $config->{relay}->{$nr}->{name}
-            };
-            $self->render( json => $data );
-        }
-        else {
-            my $data = {
-                'error' => 0,
-                'state' => 'error',
-                'name'  => $config->{relay}->{$nr}->{name}
-            };
-            $self->render( json => $data );
-        }
-    }
-    else {
-        my $data = {
-            'error' => 1,
-            'state' => 'error',
-        };
-        $self->render( json => $data );
-    }
-};
-
-app->start;
